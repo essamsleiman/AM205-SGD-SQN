c307d674ae513809dd62377ed6eb565221b641cb
aa-mill <aaronmillerhockey21@gmail.com>
2023-12-07 16:01:23 -0500

    oBFGS and RES working

--- sqn.py
+++ sqn.py
@@ -1,7 +1,26 @@
+import numpy as np
+import matplotlib.pyplot as plt
 import torch
+import torch.nn as nn
+
+class StrongConvex(nn.Module):
+    def __init__(self, num_dimensions, num_terms):
+        super().__init__()
+        self.d = num_dimensions
+        self.n = num_terms
+        self.theta = torch.nn.Parameter(torch.rand(self.d))
+        print(f"Initial theta: {self.theta}")
+
+    def forward(self, A, b):
+        # A.shape = (n, d, d)
+        # b.shape = d
+        out = 0
+        for i in range(len(A)):
+            out += 0.5*self.theta.t()@A[i]@self.theta + b[i].t()@self.theta
+        return out
 
 class SQN(torch.optim.Optimizer):
-    def __init__(self, params, lr, delta, Gamma, Hessian) -> None:
+    def __init__(self, params, lr, delta, Gamma, Hessian):
         if delta <= 0.0:
             raise ValueError("Invalid eigenvalue bound.")
         defaults = dict(lr=lr, delta=delta, Gamma=Gamma, Hessian=Hessian)
@@ -10,7 +29,9 @@ class SQN(torch.optim.Optimizer):
         for group in self.param_groups:
             for p in group["params"]:
                 state = self.state[p]
+                state["step"] = 0
                 state["Hessian"] = torch.eye(p.numel())
+                print(f"Initial Hessian {state['Hessian']}")
 
     def step(self, closure=None):
         """
@@ -26,8 +47,8 @@ class SQN(torch.optim.Optimizer):
                     continue
                 
                 state = self.state[p]
-                if len(state) == 0:
-                    state["step"] = 0
+                # if len(state) == 0:
+                #     state["step"] = 0
                 state["step"] += 1
 
                 Hessian = state["Hessian"]
@@ -35,9 +56,9 @@ class SQN(torch.optim.Optimizer):
                 delta = group["delta"]
                 Gamma = group["Gamma"]
 
-                old_theta = p.data
+                old_theta = p.data.clone()
                 closure()
-                old_grad = p.grad.data
+                old_grad = p.grad.data.clone()
 
                 theta_update = -lr*(torch.inverse(Hessian) + Gamma*torch.eye(Hessian.size(0)))@old_grad
                 p.data.add_(theta_update)
@@ -49,11 +70,62 @@ class SQN(torch.optim.Optimizer):
                 v = new_grad - old_grad - delta*u
 
                 Hessian_update = torch.outer(v, v)/torch.dot(u, v) \
-                    - torch.outer(torch.mv(Hessian, u), torch.matmul(torch.T(u), Hessian))/torch.dot(u, torch.mv(Hessian, u)) \
+                    - torch.outer(torch.mv(Hessian, u), torch.matmul(u.t(), Hessian))/torch.dot(u, torch.mv(Hessian, u)) \
                     + delta*torch.eye(Hessian.size(0))
                 Hessian.add_(Hessian_update)
 
         return loss
     
-if __name__ == "main":
+def get_qf(num_dimension, num_terms, rng):
+    A, b = [], []
+    for i in range(num_terms):
+        A.append(np.diag(rng.uniform(1, 5, size=num_dimension)))
+        b.append(rng.uniform(-1, 1, size=num_dimension))
+    return np.array(A), np.array(b)
+
+if __name__ == "__main__":
+
+    rng = np.random.default_rng(seed=12345)
+    torch.manual_seed(12345)
+    d = 50
+    N = 1000
+    n = 5
+    lr = 0.005
+    delta = 0.1
+    Gamma = 1
+    num_epochs = 50
+
+    A, b = get_qf(d, N, rng)
+    model = StrongConvex(d, N)
+    sqn = SQN(model.parameters(), lr=lr, delta=delta, Gamma=Gamma, Hessian=np.eye(d))
+    sgd = torch.optim.SGD(model.parameters(), lr=lr)
+    optimizer = sqn
+    
+    losses = []
+    for epoch in range(num_epochs):
+
+        idxs = rng.choice(N, n, replace=False)
+        A_batch = torch.tensor(A[idxs], dtype=torch.float32)
+        b_batch = torch.tensor(b[idxs], dtype=torch.float32)
+        
+        def closure():
+            optimizer.zero_grad()
+            loss = model(A_batch, b_batch)
+            loss.backward()
+            return loss
+        
+        loss = optimizer.step(closure=closure)
+        losses.append(loss.item())
+
+        if (epoch+1) % 10 == 0:
+            print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.5f}")
+
+    fig, ax = plt.subplots()
+    ax.plot(np.arange(1, num_epochs + 1), losses, "k.-", lw=1, label="RES")
+    ax.set_ylabel(r"$f(\theta)$")
+    ax.grid()
+    ax.legend()
+    fig.tight_layout()
+    fig.savefig("losses.png", dpi=150)
+
 
