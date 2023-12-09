d2ca6f53269b0b7bb61f468f86cfa6225e2b4203
Essam Sleiman <essamfsleiman@gmail.com>
2023-12-09 13:38:26 -0500

    added code for simple ML problem, evaluated with SGD only so far

--- sqn.py
+++ sqn.py
@@ -3,6 +3,11 @@ import matplotlib.pyplot as plt
 import torch
 import torch.nn as nn
 import torch.optim.lr_scheduler as lr_scheduler
+import argparse
+import numpy as np
+from sklearn import datasets
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
 
 class StrongConvex(nn.Module):
     def __init__(self, num_dimensions, num_terms):
@@ -120,37 +125,63 @@ def f(X, Y, A, b):
             out[i, j] = temp_out
     return out
 
-if __name__ == "__main__":
 
-    # fix seeds for reproducability
-    rng = np.random.default_rng(seed=12345)
-    torch.manual_seed(12345)
-    d = 2 # problem dimension
-    N = 1000 # number of data points
-    n = 5 # batch size
-    lr = 0.01 # learning rate
-    delta = 0.1 # eigenvalue bound
-    Gamma = 1 # constant appearing in Hessian update
-    num_epochs = 5000
-
-    # obtain sample of A and b
-    A, b = get_qf(d, N, rng)
-
-    # define the loss function/model
-    model = StrongConvex(d, N)
-    sqn = SQN(model.parameters(), lr=lr, delta=delta, Gamma=Gamma, Hessian=np.eye(d))
-    sgd = torch.optim.SGD(model.parameters(), lr=lr)
-    # optimizer chooses which algorithm to use
-    optimizer = sqn
-    # scheduler decreases the learning rate by a fraction gamma every step_size
-    scheduler = lr_scheduler.StepLR(optimizer, step_size=100, gamma=0.9)
+class LogisticRegressionTask(nn.Module):
+    def __init__(self, input_dim):
+        super(LogisticRegressionTask, self).__init__()
+        # Linear layer
+        self.linear = nn.Linear(input_dim, 1)
     
-    losses = []
-    # thetas logs the parameters at every step
-    thetas = []
-    # primary training loop
-    for epoch in range(num_epochs):
+    def forward(self, x):
+        # Pass the input through the linear layer, then through sigmoid
+        return torch.sigmoid(self.linear(x))
 
+def optimize_ML(model, epochs):
+    criterion = nn.BCELoss()
+    iris = datasets.load_iris()
+    X = iris.data  # Features
+    y = iris.target  # Labels
+
+        # Convert to binary classification (Iris Setosa vs. others)
+    y = (y == 0).astype(int)
+
+    # Split the dataset into training and testing sets
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    # Feature scaling
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+
+
+    n_samples, n_features = X.shape
+    # theta = np.zeros(n_features)
+    losses = []
+    for epoch in range(epochs):
+            for j in range(0, n_samples, args.batch_size):
+                X_batch = torch.tensor(X[j:j+ args.batch_size], dtype=torch.float32)
+                y_batch = torch.tensor(y[j:j+ args.batch_size], dtype=torch.float32).unsqueeze(1)
+
+                # Forward pass
+                # print('X_batch: ', X_batch.size())
+                # print('model: ', model)
+                outputs = model(X_batch)
+                loss = criterion(outputs, y_batch)
+                # Backward and optimize
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
+
+            losses.append(loss.item())
+            print("epoch: ", epoch, "loss: ", sum(losses))
+
+    with torch.no_grad():
+        theta = model.linear.weight.detach().numpy().flatten()
+    print('theta: ', theta)
+    return theta, losses
+
+def optimize_StrongConvex(thetas, epochs):
+    for epoch in epochs:
         # randomly sample a batch of A and b to compute the stochastic gradients
         idxs = rng.choice(N, n, replace=False)
         A_batch = torch.tensor(A[idxs], dtype=torch.float32)
@@ -164,51 +195,135 @@ if __name__ == "__main__":
             return loss
         
         loss, new_theta = optimizer.step(closure=closure)
-        thetas.append(new_theta)
+        thetas.append(np.array(new_theta))
         # commenting this line is all that's needed to remove the effect of the scheduler
         scheduler.step()
 
         # print loss every few epochs
         if (epoch+1) % 10 == 0:
             print(f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item():.5f}")
-    thetas = np.array(thetas)
+    return thetas
 
+if __name__ == "__main__":
+    # Initialize the argument parser
+    parser = argparse.ArgumentParser(description="Command line arguments for training")
+
+    # Add arguments
+    parser.add_argument("--seed", type=int, default=12345, help="Random seed")
+    parser.add_argument("--dim", type=int, default=2, help="Problem dimension")
+    parser.add_argument("--data_points", type=int, default=1000, help="Number of data points")
+    parser.add_argument("--batch_size", type=int, default=5, help="Batch size")
+    parser.add_argument("--lr", type=float, default=0.01, help="Learning rate")
+    parser.add_argument("--delta", type=float, default=0.1, help="Eigenvalue bound")
+    parser.add_argument("--gamma", type=float, default=1, help="Constant in Hessian update")
+    parser.add_argument("--epochs", type=int, default=5000, help="Number of epochs")
+    parser.add_argument("--step_size", type=int, default=100, help="Step size for learning rate scheduler")
+    parser.add_argument("--lr_gamma", type=float, default=0.9, help="Gamma for learning rate scheduler")
+    parser.add_argument("--num_features", type=int, default=4, help="Number of features for ML problem")
+    parser.add_argument("--model", type=str, default="StrongConvex", choices=["StrongConvex", "ML"], help="Model to use (StrongConvex or ML)")
+    parser.add_argument("--optimizer", type=str, default="SQN", choices=["SQN", "SGD"], help="Optimizer to use (SQN or SGD)")
+
+
+    # Parse the arguments
+    args = parser.parse_args()
+
+    # fix seeds for reproducability
+    rng = np.random.default_rng(seed=args.seed)
+    torch.manual_seed(args.seed)
+    d = args.dim # problem dimension
+    N = args.data_points # number of data points
+    n = args.batch_size # batch size
+    lr = args.lr # learning rate
+    delta = args.delta # eigenvalue bound
+    Gamma = args.gamma # constant appearing in Hessian update
+    num_epochs = args.epochs
+
+    # obtain sample of A and b
+    A, b = get_qf(d, N, rng)
+
+    # define the loss function/model
+    if args.model == "StrongConvex":
+        model = StrongConvex(d, N)
+    elif args.model == "ML":
+        model = LogisticRegressionTask(args.num_features)
+    else:
+        model = None
+
+
+    if args.optimizer == "SQN":
+        optimizer = SQN(model.parameters(), lr=lr, delta=delta, Gamma=Gamma, Hessian=np.eye(d))
+    elif args.optimizer == "SGD":
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+    else:
+        optimizer = None
+
+    # optimizer chooses which algorithm to use
+    # scheduler decreases the learning rate by a fraction gamma every step_size
+    scheduler = lr_scheduler.StepLR(optimizer, step_size=args.step_size, gamma=args.lr_gamma)
+    
+    losses = []
+    # thetas logs the parameters at every step
+    thetas = []
+    # primary training loop
+    if args.model == "StrongConvex":
+        optimize_StrongConvex(thetas, args.epochs)
+        thetas = np.array(thetas)
+
+    elif args.model == "ML":
+        theta, losses = optimize_ML(model, args.epochs)
+        thetas = theta
+
+        
+    thetas = np.array(thetas)
+    print("thetas: ", thetas.shape)
     # show the exact solution
     solution, theta = get_exact(A, b)
     print(f"Analytical solution: {solution}\n{theta}")
 
     # create a contour plot for the 2D case
-    x = np.linspace(-1, 1, 25)
-    y = np.linspace(-1, 1, 25)
-    X, Y = np.meshgrid(x, y)
-    Z = f(X, Y, A, b)
-    fig, ax = plt.subplots()
-    contour = ax.contourf(X, Y, Z, 50)
-    fig.colorbar(contour)
-    ax.plot(thetas[::10, 0], thetas[::10, 1], "k.-", lw=1)
-    ax.set_xlabel(r"$\theta_1$")
-    ax.set_ylabel(r"$\theta_2$")
-    fig.tight_layout()
-    fig.savefig("contour.png", dpi=150)
+    if args.model == "StrongConvex":
+        x = np.linspace(-1, 1, 25)
+        y = np.linspace(-1, 1, 25)
+        X, Y = np.meshgrid(x, y)
+        Z = f(X, Y, A, b)
+        fig, ax = plt.subplots()
+        contour = ax.contourf(X, Y, Z, 50)
+        fig.colorbar(contour)
+        ax.plot(thetas[::10, 0], thetas[::10, 1], "k.-", lw=1)
+        ax.set_xlabel(r"$\theta_1$")
+        ax.set_ylabel(r"$\theta_2$")
+        fig.tight_layout()
+        fig.savefig("contour.png", dpi=150)
 
-    # computes the true losses given the parameter values at each step
-    for i in range(len(thetas)):
-        theta = thetas[i]
-        out = 0
-        for j in range(len(A)):
-            out += 0.5*theta.T@A[j]@theta + b[j].T@theta
-        losses.append(out/len(A))
+        # computes the true losses given the parameter values at each step
+        for i in range(len(thetas)):
+            theta = thetas[i]
+            out = 0
+            for j in range(len(A)):
+                out += 0.5*theta.T@A[j]@theta + b[j].T@theta
+            losses.append(out/len(A))
 
-    # primary plot, shows convergence of excess error vs iteration
-    errors = losses - solution
-    k = np.arange(1, num_epochs + 1)
-    fig, ax = plt.subplots()
-    ax.semilogy(k[::10], errors[::10], "k.-", lw=1, label="RES")
-    ax.set_xlabel("Iteration")
-    ax.set_ylabel(r"$f(\theta) - f(\theta^*)$")
-    ax.grid()
-    ax.legend()
-    fig.tight_layout()
-    fig.savefig("losses.png", dpi=150)
+        # primary plot, shows convergence of excess error vs iteration
+        errors = losses - solution
+        k = np.arange(1, num_epochs + 1)
+        fig, ax = plt.subplots()
+        ax.semilogy(k[::10], errors[::10], "k.-", lw=1, label="RES")
+        ax.set_xlabel("Iteration")
+        ax.set_ylabel(r"$f(\theta) - f(\theta^*)$")
+        ax.grid()
+        ax.legend()
+        fig.tight_layout()
+        fig.savefig("losses.png", dpi=150)
 
-
+    else:
+        k = np.arange(1, args.epochs + 1)
+        fig, ax = plt.subplots()
+        print("k: ", k)
+        print("losses: ", losses)
+        ax.semilogy(k, losses, "k.-", lw=1, label="RES")
+        ax.set_xlabel("Iteration")
+        ax.set_ylabel(r"$f(\theta) - f(\theta^*)$")
+        ax.grid()
+        ax.legend()
+        fig.tight_layout()
+        fig.savefig("losses.png", dpi=150)
